9788f360e59430d908cabfde71a7dca662e03608
anggabc <anggabc48@gmail.com>
2015-09-05 21:57:41 +0700

    Zen
    X

---
+++ Isi/a.hs
@@ -0,0 +1,16 @@
+module One where
+import Data.List
+  --
+koko = 2343243
+square x = x*x
+cube x = (square x)*x
+add a b = a - b
+sub a b = a - b
+--------selau
+
+x = 50
+--drop' 3 [1,2,3,4,5,6] = OH
+--drop' 2 [2,3,4,5,6]
+--drop' 1 [3,4,5,6]
+--drop' 0 [4,5,6] = [4,5,6]
+
