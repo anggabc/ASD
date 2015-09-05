e4a518ab220d1c8b5cfe21d94f6af334a5d97193
anggabc <anggabc48@gmail.com>
2015-09-05 22:33:27 +0700

    F

--- Isi/a.hs
+++ Isi/a.hs
@@ -1,19 +1,255 @@
-module One where
+module A where
 import Data.List
 
-  -- (:cd e:/code/ASD/Isi)
-  -- (:set prompt "blabla")
 
-koko = 2343243
-square x = x*x
-cube x = (square x)*x
-add a b = a - b
-sub a b = a - b
---------selau
 
-x = 50
---drop' 3 [1,2,3,4,5,6] = OH
---drop' 2 [2,3,4,5,6]
---drop' 1 [3,4,5,6]
---drop' 0 [4,5,6] = [4,5,6]
---commit suicide
+--------selau-------men-----------------------
+
+--C O R E T A N--
+-- (:cd e:/code/ASD/Isi)
+-- (:set prompt "blabla")
+-- commit suicide
+-----------------------------------------------
+
+-- First Assignment
+-- I'M NOT USING GOOGLE
+-- JUST KIDDING
+-- JUST KIDDING ^
+
+
+
+-- 1.a
+
+null' x = x
+
+--pembatas
+
+take' x = x
+
+--pembatas
+
+drop' x = x
+  |
+
+--pembatas
+
+fst' (x,y) = x
+
+--pembatas
+
+snd' (x,y) = y
+
+--pembatas
+
+map' x = x
+
+--pembatas
+
+filter' x = x
+
+--pembatas
+
+delete' x = x
+
+--pembatas
+
+deleteAll' x = x
+
+--pembatas
+
+foldl' x = x
+
+--pembatas
+
+foldl1' x = x
+
+--pembatas
+
+zip' x = x
+
+--pembatas
+
+zipWith' x = x
+
+--pembatas
+
+nth' x = x
+
+--pembatas
+
+scanl' x = x
+
+--pembatas
+
+scanl1' x = x
+
+--pembatas
+
+elem' x = x
+
+--pembatas
+
+notElem' x = x
+
+--pembatas
+
+head' x = x
+
+--pembatas
+
+length' x = x
+
+--pembatas
+
+reverse' x = x
+
+--pembatas
+
+last' x = x
+
+--pembatas
+
+tail' x = x
+
+--pembatas
+
+init' x = x
+
+--pembatas
+
+max' x = x
+
+--pembatas
+
+min' x = x
+
+--pembatas
+
+concat' x = x
+
+--pembatas
+
+intersperse' x = x
+
+--pembatas
+
+intercalate' x = x
+
+--pembatas
+
+and' x = x
+
+--pembatas
+
+or' x = x
+
+--pembatas
+
+zip3' x = x
+
+--pembatas
+
+sum' x = x
+
+--pembatas
+
+product' x = x
+
+--pembatas
+
+words' x = x
+
+--pembatas
+
+lines' x = x
+
+--pembatas
+
+unlines' x = x
+
+--pembatas
+
+unwords' x = x
+
+--pembatas
+
+takeWhile' x = x
+
+--pembatas
+
+dropWhile' x = x
+
+--pembatas
+
+concatMap' x = x
+
+--pembatas
+
+all' x = x
+
+--pembatas
+
+any' x = x
+
+--pembatas
+
+insert' x = x
+
+--pembatas
+
+zipWith3' x = x
+
+--pembatas
+
+-- 1.b
+
+nub' x = x
+
+--pembatas
+
+sort' x = x
+
+--pembatas
+
+minimum' x = x
+
+--pembatas
+
+maximum' x = x
+
+--pembatas
+
+inits' x = x
+
+--pembatas
+
+tails' x = x
+
+--pembatas
+
+union' x = x
+
+--pembatas
+
+intersect' x = x
+
+--pembatas
+
+group' x = x
+
+--pembatas
+
+splitAt' x = x
+
+--pembatas
+
+partition' x = x
+
+--pembatas
+
+replicate' x = x
+
+--pembatas
+-- First Assignment
+-- Reimplement Haskell function
+-- DON'T USE GOOGLE
